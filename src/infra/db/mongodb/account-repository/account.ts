6cd9716b072fb9aa4f56ac03ee9309fd37632e63
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2022-04-03 10:20:55 -0300

    refactor: move mongodb infra to db subfolder

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,6 +1,6 @@
-import { AddAccountRepository } from '../../../data/protocols/add-account-repository'
-import { AccountModel } from '../../../domain/models/account'
-import { AddAccountModel } from '../../../domain/usecases/add-account'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helpers'
 
 export class AccountMongoRepository implements AddAccountRepository {
