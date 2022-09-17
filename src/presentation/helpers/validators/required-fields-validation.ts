97d52e2ac208b2c78ef8f2bbb5378f381bb5e477
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2022-09-17 00:33:08 -0300

    fix: get input field with array access

--- src/presentation/helpers/validators/required-fields-validation.ts
+++ src/presentation/helpers/validators/required-fields-validation.ts
@@ -8,7 +8,7 @@ export class RequiredFieldValidation implements Validation {
   }
 
   validate (input: any): Error {
-    if (!input(this.fieldName)) {
+    if (!input[this.fieldName]) {
       return new MissingParamError(this.fieldName)
     }
 
