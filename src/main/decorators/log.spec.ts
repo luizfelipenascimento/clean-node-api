2207ad6d2763b22bc136a8b33a0e8cb0a930db1f
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2022-09-17 20:16:34 -0300

    refactor: move data layer interfaces to folders

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,7 +1,7 @@
 import { serverError, ok } from '../../presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
-import { LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import { AccountModel } from '../../domain/models/account'
 
 describe('LogController Decorator', () => {
