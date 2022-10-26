a362bdb63e5d4067b86a323a701411fa076ac3ca
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2022-10-25 23:12:01 -0300

    test: ensure AccountRepository returns null if loadByEmail fails

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -52,4 +52,10 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@email.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('any_email@email.com')
+    expect(account).toBeFalsy()
+  })
 })
