94d201358eab9f69867459f88bb55643be128360
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2022-10-31 15:15:25 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
+import { hash } from 'bcrypt'
+
+let accountCollection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -8,7 +11,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -27,6 +30,25 @@ describe('Login Routes', () => {
           passwordConfirmation: '1234'
         })
 
+      expect(response.statusCode).toBe(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should returns 200 on login', async () => {
+      const password = await hash('1234', 12)
+      await accountCollection.insertOne({
+        name: 'Luiz',
+        email: 'luiz@email.com',
+        password
+      })
+
+      const response = await request(app)
+        .post('/api/login')
+        .send({
+          email: 'luiz@email.com',
+          password: '1234'
+        })
       expect(response.statusCode).toBe(200)
     })
   })
