9128d143743d3d906c7c544eb87a4033d11e41b7
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2022-11-01 09:02:08 -0300

    refactor: change routes to use filesystem path

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,14 +1,14 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-
-const path = 'src/main/routes/'
+import path from 'path'
 
 export default (app: Express): void => {
   const router = Router()
 
   app.use('/api', router)
-
-  readdirSync(path).filter(f => /.routes\.ts/.test(f)).map(async file => {
-    (await import('../../../' + path + file)).default(router)
+  readdirSync(path.join(__dirname, '../routes/')).map(async file => {
+    if (!file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router)
+    }
   })
 }
--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,7 +1,7 @@
-import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
 import { hash } from 'bcrypt'
+import request from 'supertest'
 
 let accountCollection
 
