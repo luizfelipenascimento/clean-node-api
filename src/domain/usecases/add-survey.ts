ae3a4c546665e2e930ad8cf7adc028fe798acc8e
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2023-01-20 18:13:52 -0300

    feat: ensure AddsurveyController calls AddSurveyUsecase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,13 @@
+export interface AddSurveyModel {
+  question: string
+  answers: SurveyAnswer[]
+}
+
+export interface SurveyAnswer {
+  answer: string
+  image: string
+}
+
+export interface AddSurvey {
+  add (data: AddSurveyModel): Promise<void>
+}
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,11 +1,13 @@
 import { badRequest } from '../../../helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation, AddSurvey } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
   private readonly validation: Validation
+  private readonly addSurvey: AddSurvey
 
-  constructor (validation: Validation) {
+  constructor (validation: Validation, addSurvey: AddSurvey) {
     this.validation = validation
+    this.addSurvey = addSurvey
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -13,6 +15,10 @@ export class AddSurveyController implements Controller {
     if (error) {
       return badRequest(error)
     }
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({
+      question, answers
+    })
     return null as any
   }
 }
