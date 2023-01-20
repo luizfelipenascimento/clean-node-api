5a6cbbfda3331298faae2eb273b0b164a7bd398d
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2023-01-20 17:48:25 -0300

    feat: ensure AddSurveyController returns 400 a badRequest error if Validation fails

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
@@ -8,7 +9,10 @@ export class AddSurveyController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.validation.validate(httpRequest.body)
+    const error = await this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return null as any
   }
 }
