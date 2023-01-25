873571a28e517716333f43c953e2c1a86086fb89
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2023-01-25 11:57:33 -0300

    feat: ensure AuthMiddleware returns 403 if no headers are found

---
+++ src/presentation/middlewares/auth-middleware.ts
@@ -0,0 +1,9 @@
+import { AccessDeniedError } from '../errors'
+import { forbidden } from '../helpers/http/http-helper'
+import { HttpRequest, HttpResponse, Middleware } from '../protocols'
+
+export class AuthMiddleware implements Middleware {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    return forbidden(new AccessDeniedError())
+  }
+}
