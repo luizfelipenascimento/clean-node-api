daee71809035d5109323cbccc8882c82854740a2
Luiz Nascimento <luizfelipesilvadonascimento@gmail.com>
2023-02-04 19:40:37 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -115,5 +115,11 @@ describe('Account Mongo Repository', () => {
       expect(account.email).toBe('any_email@email.com')
       expect(account.password).toBe('any_password')
     })
+
+    test('Should return null if loadByToken fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeFalsy()
+    })
   })
 })
